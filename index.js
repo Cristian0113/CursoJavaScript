7fa0150ff36b2e7bc3458806701689ecaf372e8e
Christian.gutierrez <christian.gutierrez@arus.com.co>
2021-12-01 06:59:00 -0500

    Se sube archivo index.json

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log("test")
 const chalk = require('chalk')
-console.log(chalk.red("Hola"))
+const express = require('express')
+const app = express()
+app.listen(3000,() =>
+{
+    console.log(chalk.blue("Servidor corriendo"))
+})
